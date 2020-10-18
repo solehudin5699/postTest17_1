e6a36c369a54d9cea9332f5501ac29a2d336093b
solehudin5699 <solehudinamqi@yahoo.co.id>
2020-10-18 15:16:36 +0700

    edit

--- 3.js
+++ 3.js
@@ -2,4 +2,4 @@
 
 // users.name distance_traveled total_fares
 let query =
-  "SELECT users.name, rides.distance AS distance_traveled, (rides.distance*rides.fare) as total_fares FROM users JOIN rides ON users.id=rides.user_id LIMIT 10 OFFSET 0";
+  "SELECT users.name, rides.distance AS distance_traveled, (rides.distance*rides.fare) as total_fares FROM users JOIN rides ON users.id=rides.user_id ORDER BY total_fares DESC LIMIT 10 OFFSET 0";
